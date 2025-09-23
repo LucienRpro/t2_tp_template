53098eddd3233865a4d96488074ace5930703fb1
LucienRpro <remylucien7@gmail.com>
2025-09-23 08:57:39 +0200

    Creation branche

--- t2_tp_template/t2_tp_template.cpp
+++ t2_tp_template/t2_tp_template.cpp
@@ -4,28 +4,9 @@
 #include <iostream>
 using namespace std;
 
-template<class T>
-T maximum(T n1, T n2)
-{
-	return (n1 > n2) ? n1 : n2;
-}
 int main()
 {
-	int i1, i2;
-	float f1, f2;
-	char c1, c2;
 
-	cout << "Test d'une fonction de comparaison 'template'" << endl;
-
-	cout << "Entrez les valeurs de i1 et i2" << endl;
-	cin >> i1 >> i2;;
-	cout << maximum(i1, i2) << endl;
-	cout << "Entrez les valeurs de f1 et f2" << endl;
-	cin >> f1 >> f2;;
-	cout << maximum(f1, f2) << endl;
-	cout << "Entrez les valeurs de c1 et c2" << endl;
-	cin >> c1 >> c2;;
-	cout << maximum(c1, c2) << endl;
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
